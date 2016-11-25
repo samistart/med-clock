6f32e169226cd67893b7fbae227798f91a61e938
samistart <samistart@hotmail.com>
2016-11-25 21:54:24 +0000

    updated data models still needs foreign keys

--- models.py
+++ models.py
@@ -10,8 +10,28 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 
+class Experiment(Base):
+    __tablename__ = 'Experiment'
+    id = Column('Id', Integer(), primary_key=True, nullable=False)
+    name = Column('Name', String(), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Patient(Base):
+    __tablename__ = 'Patient'
+    id = Column('Id', Integer(), primary_key=True, nullable=False)
+    age = Column('Age', Integer(), nullable=False)
+    attributes = Column('Age', String(), nullable=True)
+
+    def __init__(self, age, attributes):
+        self.age = age
+        self.attributes = attributes
+
+
 class Stage(Base):
-    __tablename__ = 'Entry'
+    __tablename__ = 'Stage'
     experiment_id = Column('Experiment Id', Integer(), primary_key=True, nullable=False)
     patient_id = Column('Patient Id', Integer(), primary_key=True, nullable=False)
     stage = Column('Stage', Integer(), primary_key=True, nullable=False)
