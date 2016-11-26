2b3bc8ba3c682314915a672858c832d41821f153
Bouke Nederstigt <bouke.nederstigt@gmail.com>
2016-11-26 19:50:11 +0000

    Client connected to api

--- raspberry/med-clock-client/client.py
+++ raspberry/med-clock-client/client.py
@@ -1,7 +1,32 @@
 import bluetooth
-from time import time
+import time
+from tornado.httpclient import HTTPClient, HTTPRequest
+from urllib import urlencode
+
 
 devices = {}
+
+def enterStage(addr, timestamp):
+        client = HTTPClient()
+
+        body = {"experiment_id": 1, "patient_id": 1, "stage_id": 1, "patient": addr, "timestamp": timestamp}
+
+
+        response = client.fetch("http://127.0.0.1:5000/api/stage/1",method="POST", body=urlencode(body), headers=urlencode({}))
+
+        print(response.body)
+        client.close()
+        return
+
+def leaveStage(addr, timestamp):
+        client = HTTPClient()
+        body = {"experiment_id": 1, "patient_id": 1, "stage_id": 1, "patient": addr, "timestamp": timestamp}
+
+        response = client.fetch("http://127.0.0.1:5000/api/stage/1",method="POST", body=urlencode(body), headers=urlencode({}))
+
+        print(response.body)
+        client.close()
+        return
 
 def scan():
         timeStamp = int(time.time())
@@ -12,10 +37,12 @@ def scan():
                 if(addr not in devices):#add new device
                         devices[addr] = (name, timeStamp)
                         print("new device:   %s - %s added on: %s" % (addr, name, str(devices[addr][1])))
+                        enterStage(addr, timeStamp)
                 else:
                         #update timestime
                         devices[addr]=(name, timeStamp)
                         print("updated device: %s - %s, new time: %s" % (addr, name, str(devices[addr][1])))
+                        leaveStage(addr,timeStamp)
         return timeStamp
 
 def remove_before_time(timeStamp):
@@ -25,7 +52,7 @@ def remove_before_time(timeStamp):
                 if data[1] < timeStamp:
                         print("removing device: %s - %s, addedTime: %s" % (device, data[0], data[1]))
                         del devices[device]
-                
+
 def main():
         while(True):
                 scanTime = scan()
