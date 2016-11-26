37fcfcb3c07a00450e4a998ee835591f790f4ec6
Bouke Nederstigt <bouke.nederstigt@gmail.com>
2016-11-26 17:05:12 +0000

    Bluetooth scanner client

---
+++ raspberry/med-clock-client/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+import os
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='med-clock-client',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='Client for med clock',
+
+    # Author details
+    author='5bitz',
+
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'deploy']),
+    entry_points = {
+        'console_scripts': ['med-clock-client=app:main'],
+    },
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=reqs
+
+)
+
+
+
+
